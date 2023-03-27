05f13cd7752b26193c2b357f7b83095a5de4d15a
msopyan <faqihsopyan@gmail.com>
2023-03-27 14:40:17 +0700

    2 files modified, 1 added

--- src/main.ts
+++ src/main.ts
@@ -8,38 +8,42 @@ import { bootstrapApplication } from '@angular/platform-browser';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <nav class="navbar navbar-black bg-black">
-      <div class="container-fluid">
-        <a class="navbar-brand">Langitku</a>
-        <form class="d-flex align-items-center">
-          <div class="dropdown-small me-2">
-            <button class="btn btn-sm btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-              IDN
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              <li><a class="dropdown-item" href="#">Indonesia</a></li>
-              <li><a class="dropdown-item" href="#">English</a></li>
-            </ul>
-          </div>
-          <button class="btn btn-outline-light text-uppercase br-12 me-2" type="submit">Login</button>
-          <button class="btn btn-pink text-uppercase br-12" type="submit">Join Us</button>
-        </form>
-      </div>
-    </nav>
+    <header class="into-header">
+      
+      <nav class="navbar navbar-black bg-black">
+        <div class="container-fluid">
+          <a class="navbar-brand">{{name}}!</a>
+          <form class="d-flex align-items-center">
+            <div class="dropdown-small me-2">
+              <button class="btn btn-sm btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                IDN
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">Indonesia</a></li>
+                <li><a class="dropdown-item" href="#">English</a></li>
+              </ul>
+            </div>
+            <button class="btn btn-outline-light text-uppercase br-12 me-2" type="submit">Login</button>
+            <button class="btn btn-pink text-uppercase br-12" type="submit">Join Us</button>
+          </form>
+        </div>
+      </nav>
     
-    <div class="container">
-      <h1>Hello from {{name}}!</h1>
-      <a target="_blank" href="https://angular.io/start">
-        Learn more about Angular 
-      </a>
-      <div class="d-flex justify-content-center py-1">
-        <button class="btn btn-primary">I'm a button</button>
-      </div>
-    </div>
+      <section class="view intro-2">
+        <div class="full-bg-img">
+          <div class="col-6 col-sm-3 py-2 px-2">
+            <h1>Inspiring today, release your master piece everywhere.</h1>
+            <div class="py-1">
+              <button class="btn btn-pink text-uppercase br-12">Discover more</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    <header>
   `,
 })
 export class App {
-  name = 'Angular';
+  name = 'Langitku';
 }
 
 bootstrapApplication(App);
